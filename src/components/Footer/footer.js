d69e49b97cd4ee11753889a4f0e9494448bd4db3
unknown <hanenkallel15@gmail.com>
2021-04-15 15:49:32 +0100

    first commit

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,205 @@
+import React from 'react';
+import {makeStyles} from "@material-ui/core/styles";
+import {Box, Grid, Paper, Typography} from "@material-ui/core";
+import styled from "styled-components";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        position: "absolute",
+        top : "900px",
+        width: "1500px",
+        height: "500px",
+        paddingTop : "50px",
+        paddingLeft : "10px",
+ 
+    
+        background: '#F2F2F2',
+       
+        //paddingRight: '700px',
+       
+        fontFamily: "Poppins",
+     
+       
+
+
+  },
+    title: {
+        color: '#505050',
+        fontSize: '18px',
+        marginBottom: '30px'
+    },
+    items: {
+        margin: '5px 0px',
+        color: '#707070',
+        
+    },
+    socialMedia: {
+        margin: '20px 0px',
+    },
+    copyRight: {
+        textAlign: 'center',
+        marginTop: '50px'
+    }
+}));
+
+
+const Footer = () => {
+    const item = { 
+       
+        paddintBottom : " 25px",
+        fontSize: "14px",
+        lineHeight: "150%",
+        color: "#00234B",
+        
+    }
+    const firstPhrase = {
+        /* une  question ? Besoin d'aide? Consulter notre aide en ligne ou contactez vous  */
+
+
+width: "662px",
+height: "64px",
+
+fontFamily: "Montserrat",
+fontStyle: "normal",
+fontWeight: "normal",
+fontSize: "24px",
+lineHeight: "29px",
+paddingBottom: "100px" ,
+paddingLeft : " 0px"
+
+
+
+
+
+    }
+    const copyright = { 
+        /* Rectangle 5 */
+positin : " aboslute ", 
+
+marginTop : " 100px ",
+paddingLeft : "0px",
+width : " 1500px",
+
+
+
+
+    background: "#D9D9D9",
+/* une  question ? Besoin d'aide? Consulter notre aide en ligne ou contactez vous  */
+
+
+
+
+fontFamily: "Montserrat",
+
+fontSize: "14px",
+lineHeight: "29px",
+
+color: '#000000'
+
+/* une  question ? Besoin d'aide? Consulter notre aide en ligne ou contactez vous  */
+
+
+
+
+
+
+
+
+
+    }
+    const title = { 
+
+width: "736px",
+height: "217px",
+left: "22px",
+top: "920px",
+
+fontFamily: "Montserrat",
+fontStyle: "normal",
+fontWeight: "normal",
+fontSize: "18px",
+color: '#000000'
+
+
+
+
+
+
+
+    }
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <p style={firstPhrase}> une  qusetion ? Besoins  d’aide  ? Consulter notre aide  
+                     en ligne ou <a href="/">contactez-nous </a></p>
+            <Grid container spacing={3} justify='center'>
+                <Grid item xs={12} sm={4}>
+                    <Box className={classes.paper}>
+                        <p className={classes.title}><strong style={title}>Services</strong></p>
+                        <Box className={classes.items}>
+                            <small style= { item}>Urgence </small>
+                        </Box>
+                        <Box className={classes.items}>
+                            <small style= { item}>Check Up</small>
+                        </Box>
+                        <Box className={classes.items}>
+                            <small style= { item}>Treatment of Personal Diseases</small>
+                        </Box>
+                       
+                        <Box className={classes.items}>
+                            <small style= { item}>Check Up</small>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Box className={classes.paper}>
+                        <p className={classes.title}><strong style={title}>Oral HealthServices</strong></p>
+                        <Box className={classes.items}>
+                            <small  style= { item} >Emergency </small>
+                        </Box>
+                        <Box className={classes.items}>
+                            <small  style= { item} >Check Up</small>
+                        </Box>
+                        <Box className={classes.items}>
+                            <small  style= { item}>Treatment of Personal Diseases</small>
+                        </Box>
+                    
+                        <Box className={classes.items}>
+                            <small  style= { item}>Check Up</small>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Box className={classes.paper}>
+                        <p className={classes.title}><strong style={title}>Notre adresse</strong></p>
+                        <Box className={classes.items}>
+                            <small  style= { item}>Tunisie, sfax</small>
+                        </Box>
+                        <Box className={classes.socialMedia}>
+                            <Box>
+                                <img height='40' src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/>
+                                <img height='40' src="https://img.icons8.com/fluent/48/000000/linkedin-circled.png"/>
+                                <img height='40' src="https://img.icons8.com/color/48/000000/twitter-circled--v4.png"/>
+                            </Box>
+                        </Box>
+                        <Box className={classes.items}>
+                            <img height='20' src="https://img.icons8.com/fluent/48/000000/email-open.png"/>
+                            <small  style= { item}>&nbsp; <a href = "/">hanenkallel15@gmail.com</a></small>
+                        </Box>
+                        <Box className={classes.items}>
+                            <img height='20' src="https://img.icons8.com/color/48/000000/cell-phone.png"/>
+                            <small  style= { item}>&nbsp; +216 29185212</small>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+            <Grid container spacing={0}>
+                <Grid  item xs={12}>
+                    <Typography variant='body2' style={copyright}>Copyright {new Date().getFullYear()} MEDArt All Right Reserved</Typography>
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Home/home.js
@@ -0,0 +1,167 @@
+import React from 'react'
+import styled from "styled-components";
+import dispo from "../../assets/disponibilités.png";
+import image from "../../assets/about.jpg";
+import Prenezrendez from "../../assets/Prenezrendez.png";
+import rappelsautomatiques from "../../assets/rappelsautomatiques.png"
+
+
+
+const Home = () => {
+  const imageRappel = { 
+    
+
+position: "absolute",
+width: "219px",
+height: "212px",
+left: "245px",
+top: "510px"
+/* 7062ba839841984630220fe782b10dd2 1 */
+
+
+
+
+
+  }
+  const imagePrenezrendez = {
+position: "absolute",
+width: "219px",
+height: "212px",
+left: "526px",
+top: "510px"
+  }
+  const imagedispo= {
+
+position: "absolute",
+width: "219px",
+height: "212px",
+left: "32px",
+top: "510px",
+
+
+
+
+
+
+  }
+  const imageStyle = {
+ position: "absolute",
+width: "797px",
+height: "403px",
+left: "100px",
+top: "100px",
+
+
+
+top: "10%",
+bottom: "0%",
+
+
+
+     
+
+  }
+  const mystyle = {
+  /* Pourquoi prendre rendez-vous avec Doctolib ? */
+
+position: "absolute",
+width: "570px",
+height: "44px",
+left: "50px",
+top: "490px",
+
+fontFamily: "Montserrat",
+fontStyle: "normal",
+fontWeight: "normal",
+fontSize: "24px",
+lineHeight: "29px",
+
+color: "#000000"
+
+
+  };
+  const parag1 = {
+    /* Accédez aux disponibilités de dizaines de milliers de professionnels de santé. */
+
+position: "absolute",
+width: "219px",
+height: "189px",
+left: "35px",
+top: "751px",
+
+fontFamily: "Montserrat",
+fontStyle: "normal",
+fontWeight: "normal",
+fontSize: "14px",
+lineHeight: "17px",
+
+color: "#000000"
+
+
+
+  }
+  const parag2 = {
+    /* Accédez aux disponibilités de dizaines de milliers de professionnels de santé. */
+
+position: "absolute",
+width: "219px",
+height: "189px",
+left: "290px",
+top: "751px",
+
+fontFamily: "Montserrat",
+fontStyle: "normal",
+fontWeight: "normal",
+fontSize: "14px",
+lineHeight: "17px",
+
+color: "#000000"
+/* Prenez rendez vous en ligne, 24h/24 et 7j/7, pour une consultation physique */
+
+
+
+
+
+  }
+  const parag3 = {
+    /* Accédez aux disponibilités de dizaines de milliers de professionnels de santé. */
+
+position: "absolute",
+width: "219px",
+height: "189px",
+left: "538px",
+top: "751px",
+
+fontFamily: "Montserrat",
+fontStyle: "normal",
+fontWeight: "normal",
+fontSize: "14px",
+lineHeight: "17px",
+
+color: "#000000"
+
+
+
+
+  }
+  return (
+    <div>
+    <Image style={imageStyle} src={image} alt="Company Logo" />
+    <p style={mystyle}>Pourquoi prendre rendez-vous avec MEDArt ?</p>
+    <Image style ={imagedispo} src={dispo}/>
+    <Image style = {imagePrenezrendez} src = {Prenezrendez}/>
+    <Image style = {imageRappel} src = {rappelsautomatiques}/>
+   <p style={parag1}>Accédez aux disponibilités de dizaines de milliers de professionnels de santé.</p>
+  <p style={parag2}>Prenez rendez vous en ligne, 24h/24 et 7j/7, pour une consultation physique</p>
+  <p style={parag3}>Recevez des rappels automatiques par SMS ou par email.</p>
+    
+    </div>
+  )
+}
+
+export default Home
+
+
+const Image = styled.img`
+
+`;
